95fbf8005f5f827c7f6fc9ef430161308d748bf2
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2024-12-04 18:45:17 +0700

    change url

--- g02_be/src/user/user.controller.ts
+++ g02_be/src/user/user.controller.ts
@@ -38,6 +38,6 @@ export class UserController {
   async googleAuthRedirect(@Req() req: ExpressRequest, @Res() res: Response) {
     const user = req.user; // Thông tin người dùng từ Google
     const {token} = await this.userService.handleGoogleUser(user);
-    return res.redirect(`http://localhost:3000/login-success?token=${token}`);
+    return res.redirect(`${process.env.FRONTEND_URL}/login-success?token=${token}`);
   }
 }
